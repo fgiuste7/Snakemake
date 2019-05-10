35f5824fe7d1853dc3cd0c15783d5103da0a7eac
Unknown <fgiuste@emory.edu>
2019-05-10 16:21:56 -0400

    helloworld

--- snakemake_HelloWorld.py
+++ snakemake_HelloWorld.py
@@ -3,26 +3,35 @@
 # Snakefile
 # For Testing Snakemake
 
-import glob
-import os
+import glob, os
 
 #-------------------------------------------------------------------------------
 # Config
 #-------------------------------------------------------------------------------
-
-
+# Obtain sample names from input directory contents:
+SAMPLES = [fname.split('/')[-1] for fname in glob.glob('./testdir/input/*')]
+GOODBYES = expand("{sample}/goodbye/goodbye.txt", sample = SAMPLES)
 
 
 #-------------------------------------------------------------------------------
 # Rules
 #-------------------------------------------------------------------------------
-localrules: hello_world
+localrules: all, make_goodbyes
 
-rule hello_world:
-    input: 
+rule all:
+    input: GOODBYES
     output: 
     shell:
         """
         echo "Hello World"
         """
 
+rule make_goodbyes:
+    input: "testdir/input/{sample}"
+    output: "{sample}/goodbye/goodbye.txt"
+    shell:
+        """
+        echo {wildcards.sample}
+        echo {input}
+        touch {output}
+        """
