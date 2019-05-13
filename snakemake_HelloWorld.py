0bf106d17bc951ed8b34109e97810bffb860430b
Unknown <fgiuste@emory.edu>
2019-05-13 13:59:17 -0400

    fix

--- snakemake_HelloWorld.py
+++ snakemake_HelloWorld.py
@@ -10,7 +10,7 @@ import glob, os
 #-------------------------------------------------------------------------------
 # Obtain sample names from input directory contents:
 SAMPLES = [fname.split('/')[-1].split('.txt')[0] for fname in glob.glob('./testdir/input/*')]
-GOODBYES = expand("testdir/{sample}/output/goodbye.txt", sample = SAMPLES)
+GOODBYES = expand("testdir/output/{sample}/goodbye.txt", sample = SAMPLES)
 
 
 #-------------------------------------------------------------------------------
