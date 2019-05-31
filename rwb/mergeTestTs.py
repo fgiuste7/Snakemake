da5040ca29881a74871f0027d405553986f1b1cf
Unknown <fgiuste@emory.edu>
2019-05-31 12:36:45 -0400

    mergeTestTs

---
+++ rwb/mergeTestTs.py
@@ -0,0 +1,66 @@
+# mergeTestTs(randout, mergeout, contrast): Merge test T-statistic files across chunks into single file for each contrast
+# By: Felipe Giuste
+
+### Command Line Argument Processing ###
+import sys
+print(sys.argv)
+if(len(sys.argv) == 4):
+    randout = sys.argv[1]
+    mergeout = sys.argv[2]
+    ncontrasts = int(sys.argv[3])
+    print("randout: %s"%(randout,))
+    print("mergeout: %s"%(mergeout,))
+    print("ncontrasts: %s"%(ncontrasts,))
+else:
+    print("Incorrect number of arguments, should be 3\nWas: %s" % (len(sys.argv)-1))
+    exit()
+
+
+# Function grabs data matrix within nifti file (no header):
+def getNII(nii):
+    import nibabel as nib
+    import numpy as np
+    image_nii = nib.nifti1.load(nii)
+    data_nii = image_nii.get_fdata(dtype=np.float64)
+    image_nii.uncache()
+    return(data_nii)
+
+
+# Merge test T-statistic files across chunks into single file:
+def mergeTestTs(randout, mergeout, contrast):
+    import numpy as np
+    from glob import glob
+    import re, zarr
+    zarrf= '%s/TestTs_%s.zarr' % (mergeout, contrast)
+    print('Merging Test-T Values from contrast: %s' % contrast) 
+    
+    # randomise results: randout/rowSlice/chunk/chunk*
+    testTs= glob( '%s/*/*/*[0-9]_tstat%s.nii.gz' % (randout, contrast))
+    
+    filedict= {}
+    for ifile in testTs:
+        # RowStart-RowEnd_ColStart-ColEnd (inclusive)
+        research= re.search( '.*/(.*?)-(.*?)_(.*?)-(.*?)/', ifile )
+        chunk_coords= [research.group(1), research.group(2), research.group(3), research.group(4)]
+        chunk_coords= [int(k) for k in chunk_coords]
+        filedict[ifile]= chunk_coords
+    
+    tmp= np.array( list(filedict.values()) )
+    tmp= np.max( tmp, axis=0)
+    shape= [ tmp[i]+1 for i in [1,3] ] # shape of final array needs to include maximums for source and target
+    
+    # create zarr file to output final matrix (2D)
+    testTs= zarr.zeros(store= zarrf, shape=shape, chunks=(100,100))
+    
+    # loop through dictionary of input files to assign final values to testT:
+    for i,j in filedict.items():
+        tmpnii=getNII(i)
+        # Assaign nifti test T-values to testTs:
+        # Add one because chunk coordinates from filename are inclusive:
+        testTs.oindex[ j[0]:j[1]+1, j[2]:j[3]+1 ]=tmpnii
+    print('### Test T-values Merged: %s' % zarrf )
+
+
+# Merge test T-statistic files across chunks into single file for a contrast:
+for contrast in range(1,ncontrasts+1):
+    mergeTestTs(randout, mergeout, contrast)
