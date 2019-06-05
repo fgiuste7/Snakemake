be37df72ff16e4bbf77b18e321dd7fb146d96820
Unknown <fgiuste@emory.edu>
2019-06-05 19:35:50 -0400

    T2Pvals + Snakefile (all niftis)

--- rwb/T2Pvals.py
+++ rwb/T2Pvals.py
@@ -1,8 +1,6 @@
 # T2Pvals(tstatout, ncontrasts): Calculate voxel-wise corrected P-Value
 # By: Felipe Giuste
 
-import numpy as np
-
 ### Command Line Argument Processing ###
 import sys
 print(sys.argv) # Index 0 lists all arguments
@@ -23,8 +21,9 @@ else:
 # qtable: quantile table (2 columns)
 # 1st column: NullTs
 # 2nd column: percentage of NullTs <= first column value
+from dask import delayed
+import numpy as np
 def getAlpha(x, qtable):
-    import numpy as np
     if(x > qtable[-1, 0]):
         return(0)
     if(x <= qtable[0, 0]):
@@ -32,8 +31,9 @@ def getAlpha(x, qtable):
     xless = x > qtable[:, 0]
     p_val = 1 - qtable[xless][-1, 1]
     return(p_val)
-
 getAlpha_vect = np.vectorize(getAlpha, excluded=('qtable',))
+gAv= delayed(getAlpha_vect, pure=True)
+
 
 def T2Pvals(tstatout, nulltstatout, pval_dir, ncontrasts):
     import numpy as np
@@ -42,7 +42,7 @@ def T2Pvals(tstatout, nulltstatout, pval_dir, ncontrasts):
 
     for contrast in range(1, ncontrasts+1):
         # open zarr file containing Test T-values
-        testTs = da.from_zarr('%s/TestTval_%s.zarr' % (tstatout, contrast))
+        testTvals = da.from_zarr('%s/TestTval_%s.zarr' % (tstatout, contrast))
         nullTs = np.genfromtxt('%s/NullTCounter_%s.csv' % (nulltstatout, contrast))
 
         # Process NullTs:
@@ -70,15 +70,34 @@ def T2Pvals(tstatout, nulltstatout, pval_dir, ncontrasts):
         T_cutoff_95 = nullTs_quantile[nullTs_quantile[:, 1] > 0.95][0]
         print('alpha=0.05 T-Value: %s' % T_cutoff_95)
 
-        p_values = da.apply_gufunc(getAlpha_vect, x=testTs, qtable=nullTs_quantile,
-                                   signature="(),(j,k)->()",
-                                   output_dtypes=float)
+        x_chunks = testTvals.numblocks[0]
+        y_chunks = testTvals.numblocks[1]
+        # List of delayed getAlpha() on chunks:
+        jobs=[]
+        # Iterate accross chunks:
+        for i in range(x_chunks):
+            for j in range(y_chunks):
+                # delayed getAlpha() on chunk:
+                jobs.append( gAv(testTvals.blocks[i,j], qtable=nullTs_quantile) )
+        
+        # Convert delayed function calls into returned arrays:
+        jobs1 = [da.from_delayed(j, shape=testTvals.chunksize, dtype=np.float) for j in jobs]
+        # Iterate accross jobs1 list of arrays and reformat into final array:
+        for i in range(x_chunks):
+            start= i * y_chunks
+            end= start+ y_chunks
+            try: 
+                # Concatenate jobs1 corresponding to single row:
+                p_values = da.concatenate( (p_values, da.concatenate(jobs1[start:end], axis=1)), axis=0 )
+            except NameError:
+                # Concatenate jobs1 corresponding to single row:
+                p_values = da.concatenate(jobs1[start:end], axis=1 )
 
         print('contrast: %s' % contrast)
         print('p_values: %s' % p_values)
         # Create zarr file to hold p-values:
         pstore_path = '%s/corrPvals_%s.zarr' % (pval_dir, contrast)
-        pstore = zarr.create(shape=testTs.shape,
+        pstore = zarr.create(shape=testTvals.shape,
                              dtype=float,
                              store=pstore_path)
         print('pstore: %s' % pstore)
