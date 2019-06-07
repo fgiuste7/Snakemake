75996b045684742f7c15ab9aa1daf5973695e25e
Unknown <fgiuste@emory.edu>
2019-06-07 10:56:00 -0400

    mergeNullTstats.py: minor

--- rwb/mergeNullTstats.py
+++ rwb/mergeNullTstats.py
@@ -48,7 +48,7 @@ def NullTmerge(contrast, randout, nulltstatout):
         writer = csv.writer(csvfile, delimiter='\t')
         for key, value in counterF.items():
             writer.writerow([key, value])
-    print('Saving: Done!' % (nulltstatout, contrast))
+    print('Saving: Done!')
     return(0)
 
 
